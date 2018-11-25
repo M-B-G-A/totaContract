0d1c987bc24e8a9ab1bc0b04c429d83ad81e1478
Jun Park <pj2417@gmail.com>
2018-11-26 02:20:48 +0900

    game 정보를 담는 테이블에 입력 추가

--- tota/tota.cpp
+++ tota/tota.cpp
@@ -7,8 +7,27 @@ using namespace eosio;
 class [[eosio::contract]] tota : public eosio::contract {
 
     public:
-        tota(name receiver, name code, datastream<const char*> ds): contract(receiver, code, ds),
-            _games(receiver, receiver.value), _histories(receiver, receiver.value) {}
+        tota(name receiver, name code, datastream<const char*> ds): contract(receiver, code, ds) {}
+
+    [[eosio::action]]
+    void insertgame(name user, std::string game_name, uint64_t game_type, uint64_t start_time,
+                     uint64_t end_time, uint64_t result_time, std::string team1, std::string team2) {
+        require_auth(user);
+        if(user.to_string() == "totagamelist") {
+            games_table games(_code, _code.value);
+            games.emplace(user, [&](auto& row) {
+                row.key = games.available_primary_key();
+                row.game_name = game_name;
+                row.game_type = game_type;
+                row.start_time = start_time;
+                row.end_time = end_time;
+                row.result_time = result_time;
+                row.result = 0;
+                row.team1 = name(team1);
+                row.team2 = name(team2);
+            });
+        }
+    }
 
     private:
         struct [[eosio::table]] game_info {
@@ -42,8 +61,7 @@ class [[eosio::contract]] tota : public eosio::contract {
         typedef eosio::multi_index<name("hitories"), user_history,
             indexed_by<name("byaccount"), const_mem_fun<user_history, uint64_t, &user_history::get_secondary_1>>
         > histories_table;
-
-        games_table _games;
-        histories_table _histories;
 };
+
+EOSIO_DISPATCH(tota, (insertgame))
 
